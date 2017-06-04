c637111919114f7a05803c178b9d289369577b5d
Carlos Santisteban <zuiko21@users.noreply.github.com>
2017-06-04 21:45:22 +0200

    68hc05 no longer supported

--- OS/zeropage.h
+++ OS/zeropage.h
@@ -2,22 +2,18 @@
 ; v0.6a2
 ; last modified 20170604-2033
 
-; *** setting these 2 bytes before will allow systmp and help HC05 stack ***
-uz_top	.byt;$DE
+; *** setting these 2 bytes before will allow systmp ***
+uz_top	.byt ;$DE
 z_used	.byt		;$DF
 
-; as 68HC05 has stack hardwired to $C0-$FF, needs special kernel with relocated locals
-#ifndef HC05
 systmp .word ; $E0
 std_in	.byt		;$E2
 stdout	.byt		;$E3
 local1	.dsb 4	;$E4
 local2	.dsb 4	;$E8
 local3	.dsb 4	;$EC
-; ...else put these elsewhere (lower RAM)
-#endif
 
-; standard minimOS-63 ABI, hardly mutable
+; standard minimOS-63 ABI
 zpar3	.dsb 4	;$F0
 zpar2	.dsb 4	;$F4
 zpar		.dsb 4	;$F8
