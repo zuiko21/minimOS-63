a5ca45995373c8bcbedc5a028ae16222938ed9e9
Carlos Santisteban <zuiko21@gmail.com>
2017-06-18 22:56:12 +0200

    rpm evaluator complete

--- forge/rpn-eval.c
+++ forge/rpn-eval.c
@@ -1,13 +1,13 @@
 /*
  * RPN expression evaluator
  * (c) 2017 Carlos J. Santisteban
- * last modified 20170618-2209
+ * last modified 20170618-2255
  */
 
 #include <stdio.h>
 
 int main(void) {
-	int i;
+	int ptr, x, y;
 
 	while(ram[ptr] != '}') {
 		x = getval(&ptr, &err);
@@ -16,5 +16,39 @@ int main(void) {
 		} else {
 			switch (ram[ptr++]) {
 			case '+':
-				x = ();=();}
+				x = pop();
+				x += pop();
+				break;
+			case '-':
+				y = pop();
+				x = pop() - y;
+				break;
+			case '*':
+				x = pop();
+				x *= pop();
+				break;
+			case '/':
+				y = pop();
+				x = pop() / y;
+				break;
+			case '&':
+				x = pop();
+				x &= pop();
+				break;
+			case '|':
+				x = pop();
+				x |= pop();
+				break;
+			case '%':
+				y = pop();
+				x = pop() % y;
+				break;
+			default:
+				abort();
+			}
+			push(x); /* computed result */
+		}
+	}
+	x = pop(); /* final result on top of stack */
+	/* might check for an empty stack, otherwise error */
 }
