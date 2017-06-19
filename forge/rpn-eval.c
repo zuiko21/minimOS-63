eea44d09f1e5e3af2c46ea2734c36b873d7681b1
Carlos Santisteban <zuiko21@users.noreply.github.com>
2017-06-19 14:44:17 +0200

    Update rpn-eval.c

--- forge/rpn-eval.c
+++ forge/rpn-eval.c
@@ -1,15 +1,55 @@
 /*
  * RPN expression evaluator
  * (c) 2017 Carlos J. Santisteban
- * last modified 20170618-2255
+ * last modified 20170619-1445
  */
 
 #include <stdio.h>
 
-int main(void) {
-	int ptr, x, y;
+/* Global variables */
 
-	while(ram[ptr] != '}') {
+unsigned char ram[65536];
+/* stack structure */
+int stack[256];
+int sp = 0;
+
+void push(int x) {
+	if (sp<256)	stack[sp++] = x;
+	else		printf("\n*** FULL STACK ***\n");
+}
+
+int pop(void) {
+	if (sp>0)	return stack[--sp];
+	else		printf("\n*** EMPTY STACK ***\n");
+	return 0;
+}
+
+bool isvalid(int *p) {
+	bool result = FALSE;	/* bad by default */
+	
+	while (ram[*p]
+	       
+//**********************************************	       
+int getval(int *p, bool *e) {
+	int value = 0;
+	*e = TRUE;	/* error unless some number detected */
+	
+	while (isvalid(*p)) {	/* valid cipher */
+		*e = FALSE;	/* no longer an error */
+		value *= 10;	/* previous cipher */
+		value += ram[p]-'0';	/* add numeric value */
+		*p++;			/* go for next, if possible */
+	}
+	
+	return value;
+}
+
+int main(void) {
+	int ptr = 0, x, y;
+	
+	scanf(">%s ", &(ram[ptr]));	/* input string */
+
+	while (ram[ptr] != '}') {
 		x = getval(&ptr, &err);
 		if (!err) {
 			push(x); /* was a number */
