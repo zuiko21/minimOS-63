26154f2b699c32ae65eec5b5259c3be22a7afa4a
Carlos Santisteban <zuiko21@gmail.com>
2017-06-21 16:13:32 +0200

    stupid V...

--- OS/sysvars.h
+++ OS/sysvars.h
@@ -1,6 +1,6 @@
 ; minimOS·63 0.6a2 System Variables
 ; (c) 2012-2017 Carlos J. Santisteban
-; last modified 20170621-1354
+; last modified 20170621-1412
 
 ; **** I/O management ****
 ; ** pointer tables for drivers, new order suggested for alternative version **
@@ -28,7 +28,7 @@ drv_freq	RMB MAX_QUEUE		; array of periodic task frequencies (word?)
 drv_async	RMB MAX_QUEUE		; space for async task pointers
 drv_r_en	RMB MAX_QUEUE		; interleaved array of async interrupt task flags
 drv_p_en	EQU drv_r_en+1		; ditto for periodic tasks (interleaved)
-drv_count	RMV	MAX_QUEUE		; eeeeeeeeeeeeeeeeeeeeek
+drv_count	RMB	MAX_QUEUE		; eeeeeeeeeeeeeeeeeeeeek
 
 ; *** single-task sigterm handler separate again! ***
 ; multitasking should provide appropriate space!
